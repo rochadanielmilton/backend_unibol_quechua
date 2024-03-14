ea82e92d6e766e3eb3cc44b708dd72f9db0e7c2c
rocha.daniel <rochagarciadaniel@gmail.com>
2024-03-14 17:00:14 -0400

    commit

--- reportes/views.py
+++ reportes/views.py
@@ -13,14 +13,53 @@ from django.db.models import Max
 # Create your views here.
 @api_view(['GET'])
 def EstudiantesCarreraAnio(request,codigo_carrera,anio_cursado):
-    estudiante=Estudiante.objects.filter(codigo_carrera=codigo_carrera,anio_cursado=anio_cursado, inscrito_gestion='si')
-    nombre_carrera=Carrera.objects.filter(codigo_carrera=codigo_carrera).first().nombre_carrera
-    if estudiante:
-        numero_estudiantes=estudiante.count()
-        estudiante_serializer=EstudianteCarreraAnioSerializer(estudiante,many=True).data
-        return Response({"año":anio_cursado,
-                        "carrera":nombre_carrera,
-                        "numero_estudiantes":numero_estudiantes,
-                        "estudiantes":estudiante_serializer})
-    else:
-        return Response({"message":"no se encontro ningun estudiante ingrese datos validos"})
+    try:
+        if codigo_carrera==0 and anio_cursado==0:
+            estudiante = Estudiante.objects.filter(inscrito_gestion='si', anio_cursado__in=['PRIMER AÑO', 'SEGUNDO AÑO', 'TERCER AÑO', 'CUARTO AÑO', 'QUINTO AÑO']).order_by('anio_cursado')
+            nombre_carrera='TODOS'
+            numero_estudiantes=estudiante.count()
+            anio_cursado='TODOS'
+            return Response({"año":anio_cursado,
+                            "carrera":nombre_carrera,
+                            "numero_estudiantes":numero_estudiantes,
+                            "estudiantes":estudiante_serializer})
+    
+        if codigo_carrera!=0 and anio_cursado==0:
+            estudiante=Estudiante.objects.filter(codigo_carrera=codigo_carrera, inscrito_gestion='si',anio_cursado__in=['PRIMER AÑO', 'SEGUNDO AÑO', 'TERCER AÑO', 'CUARTO AÑO', 'QUINTO AÑO']).order_by('anio_cursado')
+            estudiante_serializer=EstudianteCarreraAnioSerializer(estudiante,many=True).data       
+            nombre_carrera=Carrera.objects.filter(codigo_carrera=codigo_carrera).first().nombre_carrera
+            numero_estudiantes=estudiante.count()
+            anio_cursado='TODOS'
+            return Response({"año":anio_cursado,
+                            "carrera":nombre_carrera,
+                            "numero_estudiantes":numero_estudiantes,
+                            "estudiantes":estudiante_serializer})
+    
+        if codigo_carrera!=0 and anio_cursado!=0:
+            estudiante=Estudiante.objects.filter(codigo_carrera=codigo_carrera,anio_cursado=anio_cursado, inscrito_gestion='si').order_by('anio_cursado')
+            estudiante_serializer=EstudianteCarreraAnioSerializer(estudiante,many=True).data
+            nombre_carrera=Carrera.objects.filter(codigo_carrera=codigo_carrera).first().nombre_carrera
+            numero_estudiantes=estudiante.count()
+
+            return Response({"año":anio_cursado,
+                            "carrera":nombre_carrera,
+                            "numero_estudiantes":numero_estudiantes,
+                            "estudiantes":estudiante_serializer})
+    except:
+        return Response({"message":"vuelva a ingresar correctamente los valores"})
+    
+# @api_view(['GET'])
+# def EstudiantesInscritosGenero(request):
+#     reporte=[]
+
+#     estudiante=Estudiante.objects.filter(codigo_carrera='AGRF',genero=genero, inscrito_gestion='si')
+#     nombre_carrera=Carrera.objects.filter(codigo_carrera=codigo_carrera).first().nombre_carrera
+#     if estudiante:
+#         numero_estudiantes=estudiante.count()
+#         estudiante_serializer=EstudianteCarreraAnioSerializer(estudiante,many=True).data
+#         return Response({"año":anio_cursado,
+#                         "carrera":nombre_carrera,
+#                         "numero_estudiantes":numero_estudiantes,
+#                         "estudiantes":estudiante_serializer})
+#     else:
+#         return Response({"message":"no se encontro ningun estudiante ingrese datos validos"})
